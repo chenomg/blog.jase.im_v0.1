bd476731bf4a67be21b0ab3db02dbfce29caeb17
Jase Chen <xxmm@live.cn>
2018-10-19 23:53:54 +0800

    _

--- jase_im/blog/urls.py
+++ jase_im/blog/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
+from django.conf.urls import url
 from blog import views
 
+app_name = 'blog'
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^blog/', include('blog.urls')),
+    url(r'^$', views.index, name='index'),
+    url(r'^about/', views.about, name='about'),
 ]
--- jase_im/blog/views.py
+++ jase_im/blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from blog.models import Category, Tag, Post, Archive
 
-# Create your views here.
+
+def index(request):
+    posts = Post.objects.all()
+    context_dic = {'posts': posts}
+    return render(request, 'blog/index.html', context=context_dic)
+
+
+def about(request):
+    context_dic = {}
+    return render(request, 'blog/about.html', context=context_dic)
