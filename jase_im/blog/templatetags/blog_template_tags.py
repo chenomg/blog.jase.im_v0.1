d73deab0676f88519a122290e7b2e91ec6f60c4c
Jase Chen <xxmm@live.cn>
2018-11-08 23:40:47 +0800

    theme

--- jase_im/blog/templatetags/blog_template_tags.py
+++ jase_im/blog/templatetags/blog_template_tags.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 from django import template
 from blog.models import Category, Post, Tag
+import math
 
 register = template.Library()
+NAME_LEN_SIDEBAR = 35
 
 
 @register.inclusion_tag('blog/show_current_categories.html')
@@ -12,7 +14,14 @@ def show_current_categories(is_detail=False, post=None):
         categories = post.category
     else:
         categories = Category.objects.all()
-    return {'categories': categories, 'is_detail': is_detail}
+        pre_half = categories[:math.ceil(categories.count() // 2 + 1)]
+        lst_half = categories[math.ceil(categories.count() // 2 + 1):]
+    return {
+        'categories': categories,
+        'pre_half': pre_half,
+        'lst_half': lst_half,
+        'is_detail': is_detail
+    }
 
 
 @register.inclusion_tag('blog/show_current_tags.html')
@@ -21,14 +30,32 @@ def show_current_tags(is_detail=False, post=None):
         tags = post.tags.all()
     else:
         tags = Tag.objects.all()
-    return {'tags': tags}
+        pre_half = tags[:math.ceil(tags.count() // 2)]
+        lst_half = tags[math.ceil(tags.count() // 2):]
+    return {
+        'tags': tags,
+        'pre_half': pre_half,
+        'lst_half': lst_half,
+    }
 
 
 @register.inclusion_tag('blog/most_viewed_posts.html')
 def most_viewed_posts():
-    return {'most_viewed_posts': Post.objects.order_by('-views')[:5]}
+    posts = Post.objects.order_by('-views')[:5]
+    for p in posts:
+        if len(p.title) > NAME_LEN_SIDEBAR:
+            p.sidebar_name = p.title[:NAME_LEN_SIDEBAR] + '...'
+        else:
+            p.sidebar_name = p.title[:NAME_LEN_SIDEBAR]
+    return {'most_viewed_posts': posts}
 
 
 @register.inclusion_tag('blog/recent_posts.html')
 def recent_posts():
-    return {'recent_posts': Post.objects.order_by('-modified_time')[:5]}
+    posts = Post.objects.order_by('-modified_time')[:5]
+    for p in posts:
+        if len(p.title) > NAME_LEN_SIDEBAR:
+            p.sidebar_name = p.title[:NAME_LEN_SIDEBAR] + '...'
+        else:
+            p.sidebar_name = p.title[:NAME_LEN_SIDEBAR]
+    return {'recent_posts': posts}
