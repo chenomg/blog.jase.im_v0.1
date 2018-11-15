ca34d5f3a9ef52389e5beda22bd29da276558aaf
Jase Chen <xxmm@live.cn>
2018-11-15 21:53:36 +0800

    _

--- jase_im/blog/urls.py
+++ jase_im/blog/urls.py
@@ -35,6 +35,3 @@ urlpatterns = [
         include('mdeditor.urls'),
     )
 ]
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- jase_im/jase_im/urls.py
+++ jase_im/jase_im/urls.py
@@ -29,6 +29,3 @@ urlpatterns = [
     url(r'^mdeditor/', include('mdeditor.urls')),
     url(r'^media/(?P<path>.*)', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-# if settings.DEBUG:
-# urlpatterns += static(
-# settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
