f25e0a7820bce2ffadeef4ed539ada800ba94eeb
Jase Chen <xxmm@live.cn>
2018-11-23 23:43:51 +0800

    css_noborder

--- paramiko.py
+++ paramiko.py
@@ -1,8 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import os
 import paramiko
+import json
+"""
+(host.key) config sample
+{
+    "hostname": "hostname/ip",
+    "port": port,
+    "username": "username",
+    "password": "password"
+}
+"""
+KEY_FILE = 'host.key'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def get_config(key_file=KEY_FILE):
+    with open(key_file, 'r') as k:
+        _key = json.load(fp=k)
+    return _key
+
+
+key = get_config()
+cmd = 'cd ' + PROJECT_DIR + ';rm -r collected_static;git fetch;git reset --hard origin/master;git pull;python3 manage.py collectstatic;service apache2 restart'
+
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.load_system_host_keys()
-client.connect(host, port, user, password)
-stdin, stdout, stderr = client.exec_command('cd %project_path%;rm -r collected_static;git fetch;git reset --hard origin/master;git pull;ls -l;python3 manage.py collectstatic;ls -l;service apache2 restart')
+client.connect(key.hostname, key.port, key.username, key.password)
+stdin, stdout, stderr = client.exec_command(cmd)
+print(stdout.read().decode('utf-8'))
