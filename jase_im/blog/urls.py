a53d48c0907f06594ec78adc0d42ae237506e081
Jase Chen <xxmm@live.cn>
2019-02-25 23:41:20 +0800

    api

--- jase_im/blog/urls.py
+++ jase_im/blog/urls.py
@@ -21,20 +21,23 @@ from blog import views
 app_name = 'blog'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^about/', views.about, name='about'),
-    url(r'^tags/', views.tag_list_show, name='tag_list_show'),
-    url(r'^tag/(?P<tag_slug>.+)/', views.tag_show, name='tag_show'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^tags/$', views.tag_list_show, name='tag_list_show'),
+    url(r'^tag/(?P<tag_slug>.+)/$', views.tag_show, name='tag_show'),
     url(r'^post/add/$', views.add_post, name='addpost'),
     url(r'^post/update/(?P<slug>.+)/$', views.update_post, name='update_post'),
     url(r'^post/(?P<slug>.+)/$', views.post_detail, name='post_detail'),
-    url(r'^category/', views.category, name='category'),
-    url(r'^register_profile/', views.register_profile,
+    url(r'^category/$', views.category, name='category'),
+    url(r'^register_profile/$', views.register_profile,
         name='register_profile'),
-    url(r'^comment_submit/', views.comment_submit, name='comment_submit'),
-    url(r'^archive/', views.archive, name='archive'),
-    url(r'^user/(?P<username>.+)/', views.user_show, name='user_show'),
+    url(r'^comment_submit/$', views.comment_submit, name='comment_submit'),
+    url(r'^archive/$', views.archive, name='archive'),
+    url(r'^user/(?P<username>.+)/$', views.user_show, name='user_show'),
     url(
         r'^mdeditor/',
         include('mdeditor.urls'),
-    )
+    ),
+    # api
+    url(r'^api/v1/posts/$', views.post_collection, name='api_posts'),
+    url(r'^api/v1/posts/(?P<pk>.+)/$', views.post_element, name='api_post_element'),
 ]
