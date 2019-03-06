5a4e8736994a8d198c6a745226e0013a5d412720
Jase Chen <xxmm@live.cn>
2019-03-06 23:54:28 +0800

    api

--- jase_im/api/views.py
+++ jase_im/api/views.py
@@ -1,19 +1,19 @@
+import json
+
 # from django.shortcuts import render
 import requests
-import json
+from rest_framework.views import APIView
+from rest_framework import viewsets
+from rest_framework.response import Response
 
 # Create your views here.
 
 
-def bing_daily_wallpaper():
-    request_url = 'http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN'
-    base_url = 'http://s.cn.bing.net'
-    res = requests.get(request_url)
-    data = json.loads(res.content)
-    img_url = base_url + data['images'][0]['url']
-    return img_url
-
-
-if __name__ == "__main__":
-    r = bing_daily_wallpaper()
-    print(r)
+class Bing_Daily_Wallpaper(viewsets.ViewSet):
+    def get(self, request, format=None):
+        request_url = 'http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN'
+        base_url = 'http://s.cn.bing.net'
+        res = requests.get(request_url)
+        data = json.loads(res.content)
+        img_url = base_url + data['images'][0]['url']
+        return Response(img_url)
--- jase_im/jase_im/settings.py
+++ jase_im/jase_im/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'api',
     'markdown',
     'mdeditor',
     'registration',
