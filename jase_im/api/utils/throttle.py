bf00badad07a56629aa39cd16dcf01be016b0f67
Jase Chen <xxmm@live.cn>
2019-03-24 18:34:05 +0800

    api_throttle_fix

--- jase_im/api/utils/throttle.py
+++ jase_im/api/utils/throttle.py
@@ -1,34 +1,43 @@
 from rest_framework.throttling import SimpleRateThrottle
 from django.contrib.auth.models import AnonymousUser
 
-class AnonymousThrottle(SimpleRateThrottle):
+class AnonRateThrottle(SimpleRateThrottle):
     """
-    匿名用户一分钟限制访问api次数
-    """
-    scope = "AnonymousUser"
+    Limits the rate of API calls that may be made by a anonymous users.
 
-    # def allow_request(self, request, view):
-    #     if not isinstance(request.user, AnonymousUser):
-    #         return True
-    #     else:
-    #         super().allow_request(request, view)
+    The IP address of the request will be used as the unique cache key.
+    """
+    scope = 'AnonymousUser'
 
     def get_cache_key(self, request, view):
-        # 通过ip限制节流
-        return self.get_ident(request)
+        if not isinstance(request.user, AnonymousUser):
+            return None  # Only throttle unauthenticated requests.
 
-class NormalThrottle(SimpleRateThrottle):
-    """
-    普通登录用户一分钟限制访问api次数
-    """
-    scope = "NormalUser"
+        return self.cache_format % {
+            'scope': self.scope,
+            'ident': self.get_ident(request)
+        }
 
-    # def allow_request(self, request, view):
-    #     if isinstance(request.user, AnonymousUser):
-    #         return True
-    #     else:
-    #         super().allow_request(request, view)
+
+class UserRateThrottle(SimpleRateThrottle):
+    """
+    Limits the rate of API calls that may be made by a given user.
+
+    The user id will be used as a unique cache key if the user is
+    authenticated.  For anonymous requests, the IP address of the request will
+    be used.
+    """
+    scope = 'NormalUser'
 
     def get_cache_key(self, request, view):
-        # 通过ip限制节流
-        return request.user.username
+        if not isinstance(request.user, AnonymousUser):
+        # if request.user.is_authenticated:
+            ident = request.user.pk
+        else:
+            ident = self.get_ident(request)
+
+        return self.cache_format % {
+            'scope': self.scope,
+            'ident': ident
+        }
+
