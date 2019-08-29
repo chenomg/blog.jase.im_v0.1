fdfb49369da4f4dc947147bfe9b9b0480bf0c465
Jase Chen <chenomg@JasedeiMac.local>
2019-08-29 23:41:09 +0800

    ssl

--- jase_im/ssl_verify/views.py
+++ jase_im/ssl_verify/views.py
@@ -9,7 +9,10 @@ from jase_im.settings import BASE_DIR
 def ssl_verify(request, code):
     logging.info('ssl开始验证')
     file_path = os.path.join(BASE_DIR, 'ssl_verify', 'challenges', code)
-    file_open = open(file_path, 'rb').read()
+    file_open = ""
+    with open(file_path, 'w') as f:
+        f.write(code)
+        file_open = open(file_path, 'rb').read()
     logging.info('file_path: {}, file_content: {}'.format(
         file_path, file_open))
     return HttpResponse(file_open)
