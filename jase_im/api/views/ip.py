d53e1a485a8cd8b9d2c57c4585f25d65d505f1a2
Jase Chen <xxmm@live.cn>
2020-02-13 20:24:25 +0800

    api_get_ip_v4

--- jase_im/api/views/ip.py
+++ jase_im/api/views/ip.py
@@ -8,4 +8,4 @@ from blog.views import get_remote_ip
 
 class Request_IPv4(APIView):
     def get(self, request, version, format=None):
-        return HttpResponse(get_remote_ip(request), content_type='plain/txt')
+        return HttpResponse(get_remote_ip(request), content_type='text/plain')
